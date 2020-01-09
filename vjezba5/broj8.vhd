49eab4975cff14f12535b7a865dd9c9d8ba57ba5
mat1jaczyyy <mat1jaczyyy@gmail.com>
2020-01-09 22:22:32 +0100

    5. vjezba: Prosirenje 8-bitnog brojila: filtriranje signala takta

--- vjezba5/broj8.vhd
+++ vjezba5/broj8.vhd
@@ -4,6 +4,7 @@ use ieee.std_logic_unsigned.all;
 
 entity broj is
     port (
+		clk_25m: in std_logic;
 		btn_down: in std_logic;
 		led: out std_logic_vector(7 downto 0)
     );
@@ -11,9 +12,23 @@ end broj;
 
 architecture x of broj is
     signal R: std_logic_vector(7 downto 0);
+    signal clk: std_logic;
 
 begin
-	R <= R + 1 when rising_edge(btn_down);
+    upravljac: entity upravljac
+    port map (
+		Clk_key => btn_down,
+		AddrA_key => '0',
+		AddrB_key => '0',
+		ALUOp_key => '0',
+		clk_25m => clk_25m,
+		AddrA => open,
+		AddrB => open,
+		ALUOp => open,
+		Clk => clk
+    );
+	
+	R <= R + 1 when rising_edge(clk);
 
     led <= R;
 end x;
